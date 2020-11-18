e72f409f4a6c7f0d1d2674fea37f4f56705c1102
heldinho <helderferreirapassos@gmail.com>
2020-11-17 23:07:21 -0200

    fix
    Signed-off-by: heldinho <helderferreirapassos@gmail.com>

--- src/components/sidebar/bag/index.js
+++ src/components/sidebar/bag/index.js
@@ -5,7 +5,7 @@ import Product from '../../product/list'
 import './style.css'
 
 export default function Bag() {
-  const [open, setOpen] = React.useState(true)
+  const [open, setOpen] = React.useState(false)
 
   return (
     <div className={open ? 'container-bag show-header-cart' : 'container-bag'}>
@@ -20,9 +20,9 @@ export default function Bag() {
           </span>
         </div>
         <div className="list-products-bag">
-          {
-            [1, 2, 3, 4, 5, 6, 7, 8, 9].map(item => <Product />)
-          }
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((item) => (
+            <Product />
+          ))}
         </div>
         <div className="amount-bag">
           <span>
@@ -34,6 +34,12 @@ export default function Bag() {
         </div>
         <div className="btn-checkout-bag">
           <strong>Finalizar Compra</strong>
+        </div>
+        <div
+          className="btn-open-sidebar-bag"
+          onClick={() => setOpen(!open)}
+        >
+          <span className="material-icons">shopping_bag</span>
         </div>
       </div>
     </div>
